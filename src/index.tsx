7c87a638b51d0a04354bc29c7d25979442af66fe
kstulgys <karolis.stulgys@gmail.com>
2021-12-21 14:33:43 +0000

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { App } from './App';
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+const app = document.getElementById('app');
+ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  app
+);
